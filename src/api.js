bb2dddf2bee1e01d4ae1d919b34715fc9ec7378e
jxuluna <931645246@qq.com>
2020-04-26 17:32:34 +0800

    灯具部署

--- src/api.js
+++ src/api.js
@@ -14,4 +14,23 @@ export const showLighting = param => {return axios.post(`${baseUrl}api/deploy/sh
 export const addRoading = param => {return axios.post(`${baseUrl}api/deploy/addorupdateroading`,param)}
 export const selectElebox = param => {return axios.post(`${baseUrl}api/deploy/selectdeployelebox`,param)}
 export const listlamppost = param => {return axios.post(`${baseUrl}api/roadlighting/listlamppost`,param)}
-
+export const selectdeploylight = param => {return axios.post(`${baseUrl}api/deploy/selectdeploylighting`,param,{
+    headers:{
+       "Content-Type": "multipart/form-data" 
+    }
+})}
+export const selectByLighting = param => {return axios.post(`${baseUrl}api/deploy/selectbyiddeploylighting`,param,{
+    headers:{
+       "Content-Type": "multipart/form-data" 
+    }
+})}
+export const UpDateDeployLight = param => {return axios.post(`${baseUrl}api/deploy/showupdatedeploylighting`,param,{
+    headers:{
+       "Content-Type": "multipart/form-data" 
+    }
+})}
+export const ShowUpdateLighting = param => {return axios.post(`${baseUrl}api/deploy/updatedeploylighting`,param,{
+    headers:{
+       "Content-Type": "multipart/form-data" 
+    }
+})}
--- src/pages/lighting.vue
+++ src/pages/lighting.vue
@@ -41,12 +41,12 @@
         <el-button
           type="danger"
           icon="el-icon-delete"
-          @click="onSubmit"
+          @click="onDelete"
         >批量删除</el-button>
         <el-button
           type="primary"
           icon="el-icon-plus"
-          @click="onSubmit"
+          @click="isAddRoad"
         >灯具部署</el-button>
       </el-form-item>
     </el-form>
@@ -104,7 +104,7 @@
         show-overflow-tooltip
       >
       </el-table-column>
-            <el-table-column
+      <el-table-column
         prop="projectName"
         label="所属项目"
         show-overflow-tooltip
@@ -126,10 +126,104 @@
       :total="tableData.length"
     >
     </el-pagination>
+    <el-dialog
+      title="新增灯具部署"
+      :visible.sync="isAddRoad"
+    >
+      <el-form :model="form">
+        <el-form-item
+          label="控制柜" :label-width="formLabelWidth"  :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+          <el-select v-model="EleboxIds" placeholder="请选择控制柜">
+            <el-option v-for="(item,index) in allEleboxId"
+              :key="index"
+              :label="item.eleboxName"
+              :value="item.codeNumber"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="所属项目"
+          :label-width="formLabelWidth"
+        >
+          <el-input
+            v-model="mem"
+            autocomplete="off"
+          ></el-input> 
+        </el-form-item>
+        <el-form-item
+          label="单灯控制器"
+          :label-width="formLabelWidth"
+        >
+          <el-select
+            v-model="EleboxIds"
+            placeholder="请选择"
+          >
+            <el-option
+              v-for="(item,index) in allEleboxId"
+              :key="index"
+              :label="item.eleboxName"
+              :value="item.codeNumber"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="灯具管理"
+          :label-width="formLabelWidth"
+        >
+          <el-select
+            v-model="EleboxIds"
+            placeholder="请选择"
+          >
+            <el-option
+              v-for="(item,index) in allEleboxId"
+              :key="index"
+              :label="item.eleboxName"
+              :value="item.codeNumber"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="灯杆管理"
+          :label-width="formLabelWidth"
+        >
+          <el-select
+            v-model="EleboxIds"
+            placeholder="请选择"
+          >
+            <el-option
+              v-for="(item,index) in allEleboxId"
+              :key="index"
+              :label="item.eleboxName"
+              :value="item.codeNumber"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="灯杆号"
+          :label-width="formLabelWidth"
+        >
+          <el-input
+            v-model="mem"
+            autocomplete="off"
+            placeholder="请输入灯头号"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="isAddRoad = false">取消</el-button>
+        <el-button
+          type="primary"
+          @click="addRoadingId"
+        >确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getProject, showLighting } from '../api'
+import { getProject, showLighting,ShowUpdateLighting } from '../api'//selectdeploylight,selectByLighting,UpDateDeployLight,
 
 export default {
   name: 'app',
@@ -143,7 +237,20 @@ export default {
       allProject: [],
       nnlightctlProjectId: 324,
       pageNumber: 1,
-      pageSize: 10
+      pageSize: 10,
+      isAddRoad:false,
+      deployState:0,
+      nnlightctlLampModelId:'',
+      nnlightctlLamppostId:'',
+      id:'',
+      nnlightctlEleboxId:'',
+      lampHolder:'',
+      lampControllerId:'',
+      fromerEleboxId:'',
+      changeEleboxId:'',
+      fromerLamppostId:'',
+      changeLamppostId:'',
+
     }
   },
   created () {
@@ -151,27 +258,54 @@ export default {
     this.showLightingId()
   },
   methods: {
-        handleCurrentChange(val) {
-        this.pageNumber = val;
-        this.showLightingId();
-      },
-      onSubmit(){
-          this.showLightingId()
-      },
-    getProjectId() {
+    handleCurrentChange (val) {
+      this.pageNumber = val;
+      this.showLightingId();
+    },
+    onSubmit () {
+      this.showLightingId()
+    },
+    getProjectId () {
       getProject().then(data => {
         this.allProject = data.data.body.data
       })
     },
-    showLightingId() {
+    showLightingId () {
       let param = {
-        pageNumber : this.pageNumber,
-        pageSize : this.pageSize
+        pageNumber: this.pageNumber,
+        pageSize: this.pageSize
       }
       showLighting(param).then(data => {
         this.tableData = data.data.body.data
       })
-    }
+    },
+    ShowUpdateLightingId(){
+      let param= {
+        nnlightctlLampModelId:this.nnlightctlLampModelId,
+        nnlightctlLamppostId:this.nnlightctlLamppostId,
+        id:this.id,
+        nnlightctlEleboxId:this.nnlightctlEleboxId,
+        lampHolder:this.lampHolder
+      }
+      ShowUpdateLighting(param).then(data=>{
+       if (data.data.header.code === "1000") {
+          this.isAddRoad = false;
+          this.$message({
+            message: "添加成功",
+            type: "success"
+          });
+        }
+      })
+    },
+    // selectdeploylightId(){
+    //   let param = {
+    //     projectId:this.nnlightctlProjectId,
+    //   }
+    //   selectdeploylight(param).then(data =>{
+    //     if(data.data.header.code === "1000") {
+    //     }
+    //   })
+    // }
   }
 }
 </script>
